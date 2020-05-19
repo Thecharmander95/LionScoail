67ef4efcff6cf45616d1ae2b27dbefb2ad8f58a6
DJ967 <leomidili07@gmail.com>
2020-05-19 13:22:45 -0400

    Fixed the admin page error

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,13 +1,6 @@
 class AdminsController < ApplicationController
-  before_action :set_everything
-  before_action :check_admin
-
   def usersettings
+    @users = User.all
+    @conversations = Conversation.all
   end
-
-  private
-    def set_everything
-      @users = User.all
-      @conversations = Conversation.all
-    end
 end
