89b7267fb00ad607b2827ce81cb6c644228174f9
DJ967 <charmandermaster07@gmail.com>
2020-08-12 09:45:24 -0400

    Might fix everything on lionsocail for now

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
 
   def check_admin
-    if current_user.role == "admin"
+    if user_signed_in?
+      if current_user.role == "admin"
+      else
+        redirect_to root_path, notice: "You do not have access to this"
+      end
     else
       redirect_to root_path, notice: "You do not have access to this"
     end
