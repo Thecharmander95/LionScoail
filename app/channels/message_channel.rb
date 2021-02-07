ebb9ab225557aec6be6faf5c5dec2e867ba5efec
DJ967 <charmandermaster07@gmail.com>
2021-02-07 17:47:44 -0500

    fixing actioncable

--- app/channels/message_channel.rb
+++ app/channels/message_channel.rb
@@ -2,9 +2,10 @@ class MessageChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
     stream_from "message_channel"
+    current_user.appear
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+      current_user.disappear
   end
 end
