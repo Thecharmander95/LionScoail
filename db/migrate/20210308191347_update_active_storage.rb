c60be30602ee0d14a33ef7f822aff10bfe596055
DJ967 <charmandermaster07@gmail.com>
2021-03-08 14:35:23 -0500

    For heroku

--- db/migrate/20210308191347_update_active_storage.rb
+++ db/migrate/20210308191347_update_active_storage.rb
@@ -1,7 +1,5 @@
 class UpdateActiveStorage < ActiveRecord::Migration[6.1]
   def change
-      drop_table :active_storage_attachments
-      drop_table :active_storage_variant_records
       drop_table :active_storage_blobs
   end
 end
