9dac3b75c45c363a114760bd0692048083b56cbc
DJ967 <charmandermaster07@gmail.com>
2021-03-08 14:42:28 -0500

    Typeo

--- db/migrate/20210308191347_update_active_storage.rb
+++ db/migrate/20210308191347_update_active_storage.rb
@@ -1,5 +1,6 @@
 class UpdateActiveStorage < ActiveRecord::Migration[6.1]
   def change
-      drop_table :active_storage_blobs
+    drop_table :active_storage_attachments
+    drop_table :active_storage_blobs
   end
 end
