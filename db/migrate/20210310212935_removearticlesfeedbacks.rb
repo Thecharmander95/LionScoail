71b396a75889b669dca3708f1db96e22f6f876aa
DJ967 <charmandermaster07@gmail.com>
2021-03-11 18:11:18 -0500

    heroku

--- db/migrate/20210310212935_removearticlesfeedbacks.rb
+++ db/migrate/20210310212935_removearticlesfeedbacks.rb
@@ -1,7 +1,5 @@
 class Removearticlesfeedbacks < ActiveRecord::Migration[6.1]
   def change
-    remove_foreign_key "feedbacks", "users"
-    remove_foreign_key "articles", "users"
     drop_table :articles
     drop_table :feedbacks
   end
