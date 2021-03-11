7259fed0c5c7e177f76847432b2079bcf456a963
DJ967 <charmandermaster07@gmail.com>
2021-03-11 16:46:15 -0500

    Remove articles for heroku to see if it will work

--- db/migrate/20210310212935_removearticlesfeedbacks.rb
+++ db/migrate/20210310212935_removearticlesfeedbacks.rb
@@ -1,5 +1,8 @@
 class Removearticlesfeedbacks < ActiveRecord::Migration[6.1]
   def change
+    remove_foreign_key "feedbacks", "articles"
+    remove_foreign_key "feedbacks", "users"
+    remove_foreign_key "articles", "users"
     drop_table :articles
     drop_table :feedbacks
   end
