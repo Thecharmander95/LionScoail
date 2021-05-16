3b0e9b09fc97f6bc7a4ada891ff69ec524810f22
leo-midili <charmandermaster07@gmail.com>
2021-05-16 19:32:10 -0400

    Remove article and srory disable

--- app/helpers/disables_helper.rb
+++ app/helpers/disables_helper.rb
@@ -13,22 +13,8 @@ module DisablesHelper
     end
   end
 
-  def articledisable_check
-    if Disable.first.articledisable == "yes"
-      redirect_to disables_path
-    else
-    end
-  end
-
   def chatroomdisable_check
     if Disable.first.chatroomdisable == "yes"
-      redirect_to disables_path
-    else
-    end
-  end
-
-  def storydisable_check
-    if Disable.first.storydisable == "yes"
       redirect_to disables_path
     else
     end
--- db/users_schema.rb
+++ db/users_schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_16_232922) do
+ActiveRecord::Schema.define(version: 2021_05_15_160301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,8 +43,8 @@ ActiveRecord::Schema.define(version: 2021_05_16_232922) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "announcements", force: :cascade do |t|
-    t.string "text"
+  create_table "acts", force: :cascade do |t|
+    t.text "part"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -54,6 +54,17 @@ ActiveRecord::Schema.define(version: 2021_05_16_232922) do
     t.text "what"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comics", force: :cascade do |t|
+    t.string "title"
+    t.text "decrpition"
+    t.string "by"
+    t.string "number"
+    t.string "siries"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "price", null: false
   end
 
   create_table "comments", force: :cascade do |t|
@@ -71,13 +82,18 @@ ActiveRecord::Schema.define(version: 2021_05_16_232922) do
     t.integer "receiver_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["receiver_id"], name: "index_conversations_on_receiver_id"
+    t.index ["sender_id", "receiver_id"], name: "index_conversations_on_sender_id_and_receiver_id", unique: true
+    t.index ["sender_id"], name: "index_conversations_on_sender_id"
   end
 
-  create_table "disables", force: :cascade do |t|
-    t.string "postdisable"
-    t.string "conversationdisable"
-    t.string "chatroomdisable"
-    t.text "homedisable"
+  create_table "credits", force: :cascade do |t|
+    t.text "person"
+    t.text "for"
+    t.bigint "movie_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["movie_id"], name: "index_credits_on_movie_id"
   end
 
   create_table "errors", force: :cascade do |t|
@@ -87,24 +103,23 @@ ActiveRecord::Schema.define(version: 2021_05_16_232922) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
+  end
+
   create_table "helps", force: :cascade do |t|
     t.text "email"
     t.text "number"
     t.text "with"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "labouts", force: :cascade do |t|
-    t.string "title"
-    t.text "ltoppara"
-    t.text "lchange"
-    t.text "llist1"
-    t.text "llist2"
-    t.text "llist3"
-    t.text "lbuttum"
-    t.text "llinkgithub"
-    t.text "lgithubtitle"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -116,6 +131,23 @@ ActiveRecord::Schema.define(version: 2021_05_16_232922) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "author"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_movies_on_user_id"
+  end
+
+  create_table "picturescenes", force: :cascade do |t|
+    t.string "name"
+    t.bigint "movie_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["movie_id"], name: "index_picturescenes_on_movie_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -153,6 +185,16 @@ ActiveRecord::Schema.define(version: 2021_05_16_232922) do
     t.index ["name"], name: "index_rooms_on_name", unique: true
   end
 
+  create_table "scenes", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.bigint "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "actor"
+    t.index ["movie_id"], name: "index_scenes_on_movie_id"
+  end
+
   create_table "suggestions", force: :cascade do |t|
     t.string "name"
     t.bigint "user_id", null: false
@@ -173,7 +215,6 @@ ActiveRecord::Schema.define(version: 2021_05_16_232922) do
     t.string "username", null: false
     t.string "role"
     t.string "slug"
-    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_users_on_slug", unique: true
@@ -184,10 +225,14 @@ ActiveRecord::Schema.define(version: 2021_05_16_232922) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "credits", "movies"
   add_foreign_key "messages", "conversations"
   add_foreign_key "messages", "users"
+  add_foreign_key "movies", "users"
+  add_foreign_key "picturescenes", "movies"
   add_foreign_key "posts", "users"
   add_foreign_key "room_messages", "rooms"
   add_foreign_key "room_messages", "users"
+  add_foreign_key "scenes", "movies"
   add_foreign_key "suggestions", "users"
 end
