cbe2657701531ae24065f5e79d9bb5054ce0b603
leo-midili <charmandermaster07@gmail.com>
2021-07-10 13:11:55 -0400

    removed banned

--- app/mailers/all_mailer.rb
+++ app/mailers/all_mailer.rb
@@ -3,13 +3,4 @@ class AllMailer < ApplicationMailer
     @url = errors_url
     mail(to: User.all.find_by_role('admin').email, subject: 'A new error was found by a user.')
   end
-
-  def newreport
-    @url = badusers_url
-    mail(to: User.all.find_by_role('admin').email, subject: 'A user was reported.')
-  end
-
-  def help
-    mail(to: User.all.find_by_role("admin").email, subject: "Someone needs help!")
-  end
 end
--- app/models/baduser.rb
+++
@@ -1,3 +0,0 @@
-class Baduser < ApplicationRecord
-  scope :by_newest, -> { self.order(created_at: :desc) }
-end
