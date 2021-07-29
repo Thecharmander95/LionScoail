2f9ec8036e374d23bf8f9cde08a9e6f0b612fe5e
leo-midili <charmandermaster07@gmail.com>
2021-07-28 20:53:33 -0400

    fixed abouts

--- app/controllers/abouts_controller.rb
+++ app/controllers/abouts_controller.rb
@@ -4,12 +4,12 @@ class AboutsController < ApplicationController
   before_action :check_admin, only: [:new, :destroy, :edit]
 
   def new
-    @labout = About.new
+    @about = About.new
   end
 
   def create
-    @labout = About.new(about_params)
-    if @labout.save
+    @about = About.new(about_params)
+    if @about.save
       flash[:notice] = "About page was successfully created"
       redirect_to abouts_path
     else
@@ -21,7 +21,7 @@ class AboutsController < ApplicationController
   end
 
   def update
-    if @labout.update(about_params)
+    if @about.update(about_params)
      flash[:notice] = "About page was updated"
      redirect_to abouts_path
     else
@@ -32,22 +32,22 @@ class AboutsController < ApplicationController
 
   def index
     @page_title = "About Lion social"
-    @labouts = About.all
+    @abouts = About.all
   end
 
   def destroy
-    @labout.destroy
+    @about.destroy
     flash[:notice] = "About page was deleted"
     redirect_to abouts_path
   end
 
   private
 
-    def set_labout
-      @labout = About.find(params[:id])
+    def set_about
+      @about = About.find(params[:id])
     end
 
-    def labout_params
+    def about_params
       params.require(:about).permit(:title, :ltoppara, :lchange, :llist1, :llist2, :llist3, :lbuttum, :llinkgithub, :lgithubtitle)
     end
 end
