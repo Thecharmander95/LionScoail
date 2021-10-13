ae0e40a323762b54d3c046bfda6d70d70a1938b0
leo-midili <charmandermaster07@gmail.com>
2021-10-12 21:43:04 -0400

    Added a mailer

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'homes#index'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 
   resources :users
 end
