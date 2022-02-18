da0f64cd0674993c863e84799a9b0f919c3e5a8f
leo-midili <charmandermaster07@gmail.com>
2022-02-18 10:22:01 -0500

    Removed room messgaes

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
   resources :acindents
   resources :disables
   resources :announcements
-  resources :sitedisables
 
   # All resources with do and end
   resources :users do
@@ -27,10 +26,6 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  resources :rooms do
-    resources :room_messages
-  end
-
   resources :conversations, only: [:index, :create, :destroy] do
     resources :messages, only: [:index, :create]
   end
@@ -38,7 +33,6 @@ Rails.application.routes.draw do
   get :dis , "disables/change"
   get 'admins/index'
   get 'admins/users'
-  get 'admins/rooms'
   get 'admins/conversations'
   get 'admins/posts'
 
--- db/users_schema.rb
+++ db/users_schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_03_011944) do
+ActiveRecord::Schema.define(version: 2022_02_18_151403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -212,23 +212,6 @@ ActiveRecord::Schema.define(version: 2022_02_03_011944) do
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
-  create_table "room_messages", force: :cascade do |t|
-    t.bigint "room_id", null: false
-    t.bigint "user_id", null: false
-    t.text "message"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["room_id"], name: "index_room_messages_on_room_id"
-    t.index ["user_id"], name: "index_room_messages_on_user_id"
-  end
-
-  create_table "rooms", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["name"], name: "index_rooms_on_name", unique: true
-  end
-
   create_table "scenes", force: :cascade do |t|
     t.string "name"
     t.text "content"
@@ -284,8 +267,6 @@ ActiveRecord::Schema.define(version: 2022_02_03_011944) do
   add_foreign_key "movies", "users"
   add_foreign_key "picturescenes", "movies"
   add_foreign_key "posts", "users"
-  add_foreign_key "room_messages", "rooms"
-  add_foreign_key "room_messages", "users"
   add_foreign_key "scenes", "movies"
   add_foreign_key "stories", "users"
 end
