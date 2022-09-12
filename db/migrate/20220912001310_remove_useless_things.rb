5babadbe72b7809099a1f7f3197ab56c07abc6de
Thecharmander95 <charmandermaster07@gmail.com>
2022-09-11 21:47:58 -0400

    Hopefully will render correctly

--- db/migrate/20220912001310_remove_useless_things.rb
+++ db/migrate/20220912001310_remove_useless_things.rb
@@ -1,10 +1,5 @@
 class RemoveUselessThings < ActiveRecord::Migration[7.0]
   def change
-    remove_foreign_key :stories, :users
-
-    drop_table :badusers
     drop_table :comics
-    drop_table :sitedisables
-    drop_table :stories
   end  
 end
